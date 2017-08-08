37bfd65ca11e6715221c2d0b6fc168b640ec1d3a
Joseph <joseph@MacBook-Pro-de-Joseph.local>
2017-08-08 16:32:39 +0200

    ###Adjectives * new

--- MobGenerator.js
+++ MobGenerator.js
@@ -8,7 +8,8 @@ var shuffle = require('shuffle-array');
 var app = express();
 
 var names = ['Cyane', 'Joseph', 'Stéphane', 'Amélie', 'Amelberge', 'grégory', 'Igor', 'Vincent', 'Suzanne', 'Patrick', 'Nico'];
-var adjs = ['le chausse-pied', '3-tétons', 'l\'éviscérateur', 'casse bonbon', 'le nettoyeur', 'long-couteau', 'face de pet', 'aux pieds-velus'];
+var adjs = ['le chausse-pied', '3-tétons', 'l\'éviscérateur', 'casse bonbon', 'le nettoyeur', 'long-couteau', 'face de pet', 'aux pieds-velus', 'le filou','le renard','barracuda','les dents longues','raton','le musicien','le boxeur','la limande','le grand','le petit','le bègue','le gros','le maigre','le gruyère','le Marseillais','le Parisien','l incorruptible','la gâchette','le fruitier','tic-tac','le fossoyeur','le faussaire','le bleu','le rouge','le noir','le gris','le blanc','le Lyonnais','le Corse','les grands pieds','le jeune','le vieux','la sèche','la bricole','le rital','l américain','l italien','le sarde','le sicilien','le nain','le pingouin','l anglais','le tricheur','le samosa','le chinois','le chinetoc','le chanteur','la bomba','la gaufre','le magicien','porte-avions','le tatoué','le manchot','le bandit','l araignée','le cancrelat','le romano','le niçois','la volaille','le boucher','le serrurier','le lièvre','la tortue','le portugais','la pieuvre','le frelon','bouche-sèche','la sèche','l indien','la millionaire','l empereur','le crabe','le soviet','le gangster','téléphone','PMU','loto','le solitaire','tape-à-l oeil','le squelette','le batard','dos-d Azur','dos brulant','visage pâle','le chanceux','London','zozo'];
+
 
 app.use("/stylesheets", express.static(__dirname + "/stylesheets"));
 app.use("/javascript", express.static(__dirname + "/javascript"));
