eb84d4abd9a5f7eb1597ff1e12f66e2c7d4a8586
Joseph <joseph@MacBook-Pro-de-Joseph.local>
2017-08-08 09:50:22 +0200

    ###WIP DEMO

--- MobGenerator.js
+++ MobGenerator.js
@@ -2,17 +2,17 @@ var express = require('express');
 var path = require('path');
 var bodyParser = require("body-parser");
 var qs = require('qs');
-var Promise = require('Promise');
-var shuffle = require('shuffle-array'),
+var Promise = require('promise');
+var shuffle = require('shuffle-array');
 
-    var app = express();
+var app = express();
 
 var names = ['Cyane', 'Joseph', 'Stéphane', 'Amélie', 'Amelberge', 'grégory', 'Igor', 'Vincent', 'Suzanne', 'Patrick', 'Nico'];
 var adjs = ['le chausse-pied', '3-tétons', 'l\'éviscérateur', 'casse bonbon', 'le nettoyeur', 'long-couteau', 'face de pet', 'aux pieds-velus'];
 
 app.use("/stylesheets", express.static(__dirname + "/stylesheets"));
 app.use("/javascript", express.static(__dirname + "/javascript"));
-app.use(favicon(path.join(__dirname, 'images', 'favicon.ico')));
+// app.use(favicon(path.join(__dirname, 'images', 'favicon.ico')));
 app.use(bodyParser.urlencoded({
     extended: true
 }));
@@ -21,16 +21,20 @@ app.set('view engine', 'pug')
 app.get('/', function (req, res) {
     generateName()
         .then(function (mobName) {
+            console.log('will render '+ mobName);
             res.render('index', {
                 title: 'Cyanus Generator',
                 message: mobName
-            })
+            });
         });
 });
 
 
 function generateName() {
     return new Promise(function (resolve, reject) {
+        console.log('whaddup?');
         resolve(shuffle.pick(names) + ' ' + shuffle.pick(adjs));
     });
 }
+
+app.listen(1234);
