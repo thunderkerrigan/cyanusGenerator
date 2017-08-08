fd75abf08c37516eab6d0789e0d999a36b3d6086
Joseph <joseph@MacBook-Pro-de-Joseph.local>
2017-08-08 16:48:30 +0200

    ###men Names

--- MobGenerator.js
+++ MobGenerator.js
@@ -8,6 +8,7 @@ var shuffle = require('shuffle-array');
 var app = express();
 
 var names = ['Cyane', 'Joseph', 'Stéphane', 'Amélie', 'Amelberge', 'grégory', 'Igor', 'Vincent', 'Suzanne', 'Patrick', 'Nico'];
+var menNames = ['René','Alain','Georges','Michel','Boniface','Pierrot','Bertrand','Robert','Roger','Nino','Antonio','Jacques','Bernard','Bebert','René','Jo','Jojo','Cricri','Christophe','Zozo','Ernest','Coco','Auguste','Augustin','Lucien','Lulu','Albert','Amadou','André','Dédé','Bruno','Benoit','Beubeu','Claude','Cloclo','Denis','Edmont','Emilio','Emmanuel','Manu','Félix','Eugène','Florent','Gaspard','Geoffroy','Georges-Henry','Guy','Henri','Albert-Emmanuel','Guy-Alain','Hubert','Hub','Jean','Jean-Baptiste','Jean-Charles','Jean-Claude','Jean-Joseph','Jean-Louis','Jean-Marc','Jean-Marie','Jean-Maurice','Jean-Paul','Lambert','Lionel','Marcel','Moussa','Momo','Mohamed','Oscar','Pedro','Raoul','Roand','Rodolphe','Umberto','Wilfired'];
 var adjs = ['le chausse-pied', '3-tétons', 'l\'éviscérateur', 'casse bonbon', 'le nettoyeur', 'long-couteau', 'face de pet', 'aux pieds-velus', 'le filou','le renard','barracuda','les dents longues','raton','le musicien','le boxeur','la limande','le grand','le petit','le bègue','le gros','le maigre','le gruyère','le Marseillais','le Parisien','l incorruptible','la gâchette','le fruitier','tic-tac','le fossoyeur','le faussaire','le bleu','le rouge','le noir','le gris','le blanc','le Lyonnais','le Corse','les grands pieds','le jeune','le vieux','la sèche','la bricole','le rital','l américain','l italien','le sarde','le sicilien','le nain','le pingouin','l anglais','le tricheur','le samosa','le chinois','le chinetoc','le chanteur','la bomba','la gaufre','le magicien','porte-avions','le tatoué','le manchot','le bandit','l araignée','le cancrelat','le romano','le niçois','la volaille','le boucher','le serrurier','le lièvre','la tortue','le portugais','la pieuvre','le frelon','bouche-sèche','la sèche','l indien','la millionaire','l empereur','le crabe','le soviet','le gangster','téléphone','PMU','loto','le solitaire','tape-à-l oeil','le squelette','le batard','dos-d Azur','dos brulant','visage pâle','le chanceux','London','zozo'];
 
 
@@ -34,7 +35,7 @@ app.get('/', function (req, res) {
 function generateName() {
     return new Promise(function (resolve, reject) {
         console.log('whaddup?');
-        resolve(shuffle.pick(names) + ' ' + shuffle.pick(adjs));
+        resolve(shuffle.pick(names.concat(menNames)) + ' ' + shuffle.pick(adjs));
     });
 }
 
